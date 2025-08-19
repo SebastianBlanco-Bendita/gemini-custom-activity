853b2738d921dbb1a758786f7811ab03d0a8b81c
SebastianBlanco-Bendita <sebastian_blanco@benditaess.com>
2025-08-19 10:43:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-require('dotenv').config(); // Carga las variables de entorno del archivo .env
+require('dotenv').config(); // Carga las claves secretas del archivo .env
 
 const activityRoutes = require('./routes/activity');
 
 const app = express();
-
-// Middleware
 app.use(bodyParser.json());
-
-// Servir archivos estáticos (html, js, css, etc.) desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Ruta principal para la lógica de la actividad
 app.use('/activity', activityRoutes);
 
 const PORT = process.env.PORT || 3000;
